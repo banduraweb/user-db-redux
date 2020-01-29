38deef15299f8c31b1c2b50b2bc16aca21ed5cc4
bandura <banduraweb@ukr.net>
2020-01-29 10:41:27 +0200

    Updates

---
+++ precache-manifest.d6d9bdd66a86e4a4726d062f9c956d2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a6d0bb55b0ee76a7b0a37bb78658b38",
+    "url": "/users-db-redux/index.html"
+  },
+  {
+    "revision": "a00d7993637df73d2be9",
+    "url": "/users-db-redux/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "a00d7993637df73d2be9",
+    "url": "/users-db-redux/static/js/2.adb4ec39.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/users-db-redux/static/js/2.adb4ec39.chunk.js.LICENSE"
+  },
+  {
+    "revision": "43096a53ae33cc2e6f81",
+    "url": "/users-db-redux/static/js/main.721b9311.chunk.js"
+  },
+  {
+    "revision": "51356919fd321254bead",
+    "url": "/users-db-redux/static/js/runtime-main.27093e8d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.94445f14783d035accbd112c0a1b9711.js"
+  "/users-db-redux/precache-manifest.d6d9bdd66a86e4a4726d062f9c956d2c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/users-db-redux/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
