4e2a17951e5036a4559bb7250f75d0a32933c548
bandura <banduraweb@ukr.net>
2020-01-29 10:35:18 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  return (
+    <div className="App">
+    <h1>USER DB REDUX</h1>
+    </div>
+  );
+}
+
+export default App;
